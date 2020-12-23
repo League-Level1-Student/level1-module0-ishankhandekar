29eae65ceb58712532f5629901b2ffc685f59b0a
ishankhandekar <66933296+ishankhandekar@users.noreply.github.com>
2020-12-22 17:00:47 -0800

    12/22/2020

--- src/_10_cuteness_tv/cutenessTV.java
+++ src/_10_cuteness_tv/cutenessTV.java
@@ -1,5 +1,37 @@
 package _10_cuteness_tv;
 
-public class cutenessTV {
+import java.net.URI;
 
+public class cutenessTV {
+	public void run() {
+		
+	}void showDucks() {
+	      playVideo("https://www.youtube.com/watch?v=MtN1YnoL46Q");
+	 }
+
+	void showFrog() {
+	      playVideo("https://www.youtube.com/watch?v=cBkWhkAZ9ds");
+	 }
+
+	void showFluffyUnicorns() {
+	      playVideo("https://www.youtube.com/watch?v=a-xWhG4UU_Y");
+	 }
+
+	void playVideo(String videoID) {
+	     
+	     // Workaround for Linux because "Desktop.getDesktop().browse()" doesn't 
+	     //work on some Linux implementations 
+	      try {
+	      if (System.getProperty("os.name").toLowerCase().contains("linux")) {
+	      if (Runtime.getRuntime().exec(new String[] { 
+	       "which", "xdg- open" }).getInputStream().read() != -1) {
+	      Runtime.getRuntime().exec(new String[] { "xdg-open", videoID }); 
+	      }
+	      } else {
+	           URI uri = new URI(videoID);
+	           java.awt.Desktop.getDesktop().browse(uri);
+	      }      } catch (Exception e) {
+	           e.printStackTrace();
+	      }
+	}
 }
